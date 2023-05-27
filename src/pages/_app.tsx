f7cc771bce260eb31b20a073388349839dc4507f
Yijia Chen <kevinc110604@gmail.com>
2023-05-27 16:01:25 -0400

    [refactor] Leave only critical code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,16 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/react'
-
 import theme from '../theme'
 import { AppProps } from 'next/app'
-import { Provider, Client, cacheExchange, fetchExchange, ssrExchange } from 'urql';
-
-const ssrCache = ssrExchange({ isClient: true, initialState: undefined, });
+import { Provider, Client, cacheExchange, fetchExchange } from 'urql';
 
 const client = new Client({
   url: 'http://localhost:4000/graphql',
-  exchanges: [cacheExchange, ssrCache, fetchExchange ],
+  exchanges: [cacheExchange, fetchExchange ],
   fetchOptions: () => {
-    const token = "hellooooooooooooo";
     return {
-      credentials: "include",
-      url: 'http://localhost:4000/graphql',
-      "Access-Control-Allow-Credentials": true, 
-      origin: "http://localhost:3000/",
-      headers: { credentials: "include", authorization: !!token ? `Bearer ${token}` : "" },
-      // credentials: "include",
-      // headers: { "Access-Control-Allow-Credentials": true, "Access-Control-Allow-Origin": "http://localhost:3000/" },
-      // headers: {cookie: ctx && ctx.req ? ctx.req.headers.cookie : document.cookie,},
+      credentials: 'include',
+      headers: { },
+      // headers: { credentials: 'include' },
     };
   },
 });
