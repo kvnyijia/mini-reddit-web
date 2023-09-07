43d9b5380b3b07f53350d74dd39ddf695a8fa1b4
Yijia Chen <kevinc110604@gmail.com>
2023-09-07 00:03:45 -0700

    [fix] Fix hydration bugs in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from "../utils/createUrqlClient";
-import { PostsQuery, useMeQuery, usePostsQuery } from "../generated/graphql";
+import { MeQuery, PostsQuery, useMeQuery, usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
 import NextLink from "next/link";
 import { Box, Button, Flex, Heading, IconButton, Link, Stack, StackDivider, Text } from "@chakra-ui/react";
@@ -17,18 +17,40 @@ const Index = () => {
   const [{data, fetching}] = usePostsQuery({
     variables,
   });
+  const [localMeData, setLocalMeData] = useState<MeQuery>(null as any)
+  const [localData, setLocalData] = useState<PostsQuery>(null as any);
+  const [localFetching, setLocalFetching] = useState(false);
+  useEffect(() => {
+    if (data) {
+      setLocalData(data);
+    }
+  }, [data]);
 
-  if (!fetching && !data) {
-    return <div>No post exists</div>;
+  useEffect(() => {
+    setLocalFetching(fetching);
+  }, [fetching]);
+
+  useEffect(() => {
+    if (meData) {
+      setLocalMeData(meData);
+    }
+  }, [meData]);
+
+  if (!localFetching && !localData) {
+    return (
+      <Layout>
+        <div>No post exists. The server might not be running.</div>
+      </Layout>
+    );
   }
 
   return (
     <Layout>
-    {!data && fetching ? (
+    {!localData && localFetching ? (
       <div>loading...</div>
     ) : (
       <Stack spacing={8}>
-        {data!.posts.posts.map((p) => !p ? null : (
+        {localData!.posts.posts.map((p) => !p ? null : (
           <Flex 
             key={p.id} 
             p={5} 
@@ -39,14 +61,12 @@ const Index = () => {
             <UpddotSection post={p}/>
             <Box flex={1}>
               <NextLink href="/post/[id]" as={`/post/${p.id}`}>
-              {/* <Link> */}
                 <Heading fontSize="xl">{p.title}</Heading> 
-              {/* </Link> */}
               </NextLink>
               <Text>Posted by {p.creator.username}</Text>
               <Flex>
                 <Text flex={1} mt={4}>{p.textSnippet}</Text>
-                {meData?.me?.id !== p.creatorId ? null : 
+                {localMeData?.me?.id !== p.creatorId ? null : 
                   <Box ml="auto">
                     <EditDeletePostButtons id={p.id}/>
                   </Box>
@@ -57,16 +77,16 @@ const Index = () => {
         ))}
       </Stack>
     )}
-    {data && data.posts.hasMore ? (
+    {localData && localData.posts.hasMore ? (
       <Flex>
         <Button 
           onClick={() => {
             setVariables({
               limit: variables.limit,
-              cursor: data.posts.posts[data.posts.posts.length-1].createdAt,
+              cursor: localData.posts.posts[localData.posts.posts.length-1].createdAt,
             });
           }}
-          isLoading={fetching} m="auto" my={8}>
+          isLoading={localFetching} m="auto" my={8}>
           Load more
         </Button>
       </Flex>
